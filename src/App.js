53e27a0420eaf6b3c61e02a549e96c6d66852760
Andrew Liebchen <liebchen@fb.com>
2020-04-22 14:41:41 -0700

    Fix linking

--- src/App.js
+++ src/App.js
@@ -26,58 +26,69 @@ const App = () => {
         sx={{ position: "fixed", zIndex: 1, m: 3 }}
         onClick={() => setSceneId(encodeString("0:1"))}
       >
-        Home
+        {currentScene ? "Home" : "Start"}
       </Button>
-      <Scene vrModeUi={{ enabled: true }}>
-        <Entity
-          primitive="a-sky"
-          src={backgrounds[manifest.scenes[currentScene.name].background]}
-        />
-        {figma
-          .filter(
-            node =>
-              node.nodeType === "frame" &&
-              node.parent.id === decodeString(sceneId)
-          )
-          .map(plane => {
-            const planeManifest = manifest.planes[plane.name];
-
-            const buffer = new Uint8Array(Object.values(plane.bytes));
-            const src = `data:image/jpeg;base64,${Buffer.from(buffer).toString(
-              "base64"
-            )}`;
-
-            return (
-              <Entity
-                key={plane.id}
-                geometry={{
-                  primitive: "plane",
-                  width: plane.width / conversionRate,
-                  height: plane.height / conversionRate
-                }}
-                material={{ src: src }}
-                position={planeManifest.position}
-                rotation={planeManifest.rotation}
-                events={{
-                  click: () =>
-                    planeManifest.target && setSceneId(encodeString("28:31"))
-                }}
-              />
-            );
-          })}
-        <Entity primitive="a-camera">
+      {currentScene && (
+        <Scene vrModeUi={{ enabled: true }}>
           <Entity
-            primitive="a-cursor"
-            animation__click={{
-              property: "scale",
-              startEvents: "click",
-              from: "0.1 0.1 0.1",
-              to: "1 1 1",
-              dur: 150
-            }}
+            primitive="a-sky"
+            src={backgrounds[manifest.scenes[currentScene.name].background]}
           />
-        </Entity>
-      </Scene>
+          {figma
+            .filter(
+              node =>
+                node.nodeType === "frame" &&
+                node.parent.id === decodeString(sceneId)
+            )
+            .map(plane => {
+              const planeManifest = manifest.planes[plane.name];
+
+              const buffer = new Uint8Array(Object.values(plane.bytes));
+              const src = `data:image/jpeg;base64,${Buffer.from(
+                buffer
+              ).toString("base64")}`;
+
+              return (
+                <Entity
+                  key={plane.id}
+                  geometry={{
+                    primitive: "plane",
+                    width: plane.width / conversionRate,
+                    height: plane.height / conversionRate
+                  }}
+                  material={{ src: src }}
+                  position={planeManifest.position}
+                  rotation={planeManifest.rotation}
+                  events={{
+                    click: () =>
+                      planeManifest.target &&
+                      setSceneId(
+                        encodeString(
+                          figma.find(
+                            node =>
+                              node.nodeType === "page" &&
+                              node.name === planeManifest.target
+                          ).id
+                        )
+                      )
+                  }}
+                />
+              );
+            })}
+          <Entity primitive="a-camera">
+            <Entity
+              primitive="a-cursor"
+              animation__click={{
+                property: "scale",
+                startEvents: "click",
+                from: "0.1 0.1 0.1",
+                to: "1 1 1",
+                dur: 150
+              }}
+            />
+          </Entity>
+        </Scene>
+      )}
     </Box>
   );
 };
